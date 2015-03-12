8314145984d914be43f49df170ef215f0a1985fb
zac1st1k <zacfirst@gmail.com>
2015-03-12 23:37:46 +1100

    Initial Commit

---
+++ algorithmAndDataStructure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  algorithmAndDataStructure
+//
+//  Created by Zac on 12/03/2015.
+//  Copyright (c) 2015 1st1k. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
